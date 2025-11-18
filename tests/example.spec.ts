98c82356a3e6362083d21e427da6c95d494e21c9
Eimante.Bogdan <eimante.bogdan@gmail.com>
2025-11-18 16:09:54 +0200

    Initial project setup with all code files and base configuration

---
+++ tests/example.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+import { faker } from "@faker-js/faker";
+
+test.beforeEach(async ({page}) => {
+    await page.goto(process.env.APP_URL);
+})
+
+test('Verify authorization error message', async ({ page }) => {
+
+
+    const randomUsername = faker.internet.username();
+    const randomPassword = faker.internet.password();
+
+  await page.getByTestId('username-input').fill(randomUsername);
+  await page.getByTestId('password-input').fill(randomPassword);
+  await page.getByTestId('signIn-button').click();
+  await expect(page.getByTestId('authorizationError-popup-close-button')).toBeVisible();
+
+});
+
+test('Verify "Sign in" is disabled if less than 8 characters are entered to password field', async ({ page }) => {
+
+    await page.getByTestId('username-input').fill('test');
+    await page.getByTestId('password-input').fill('1234567');
+    await expect(page.getByTestId('signIn-button')).toBeDisabled();
+    await expect(page.getByText('The field must contain at least of characters: 8')).toBeVisible();
+
+});
+
+
